6835f8f0b6989ba812edbbae70c874356f5fdb12
jon <jonathan.n@u.nus.edu>
2022-12-29 05:40:32 +0000

    render from firebase

--- src/components/EntryList.tsx
+++ src/components/EntryList.tsx
@@ -1,54 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Card, CardHeader, Heading, Tag } from '@chakra-ui/react'
 import '../EntryList.css'
 import Prompt from './Prompt'
+import { Entry } from '../types/entry'
+import { getEntries } from '../firebase'
 
-function EntryList(): JSX.Element {
-    const data = [
-        {
-            template: 'abc',
-            title: 'Prompt',
-            views: 0,
-            likes: 0,
-            tags: ['potato'],
-        },
-        {
-            template: 'asdf',
-            title: 'DANK MEMES',
-            views: 1,
-            likes: 2,
-            tags: ['potato', 'tomato'],
-        },
-        {
-            template: 'Sarcasm',
-            title: 'Sarcasm',
-            views: 1,
-            likes: 2,
-            tags: ['potato', 'tomato'],
-        },
-    ]
-    const renderEntry = (entry: any) => {
+const renderEntry = (entry: Entry) => {
+    return (
+        <Card
+            size="xs"
+            color="white"
+            backgroundColor="#343541"
+            marginBottom="20px"
+            padding="20px"
+        >
+            <CardHeader display="flex" alignItems="center">
+                <Heading>{entry.title}</Heading>
+                {entry.tags.map((tag: string) => (
+                    <Tag marginLeft="5px" key={tag}>
+                        {tag}
+                    </Tag>
+                ))}
+            </CardHeader>
+            <Prompt text={entry.template} />
+        </Card>
+    )
+}
+class EntryList extends Component<{}, { entries: Entry[] }> {
+    constructor() {
+        super({})
+        this.state = {
+            entries: [],
+        }
+    }
+
+    async componentDidMount() {
+        const entries = await getEntries()
+        this.setState({ entries })
+        console.log(`Entries: ${entries.length} entries loaded.`)
+    }
+
+    render() {
         return (
-            <Card
-                size="xs"
-                color="white"
-                backgroundColor="#343541"
-                marginBottom="20px"
-                padding="20px"
-            >
-                <CardHeader display="flex" alignItems="center">
-                    <Heading>{entry.title}</Heading>
-                    {entry.tags.map((tag: string) => (
-                        <Tag marginLeft="5px" key={tag}>
-                            {tag}
-                        </Tag>
-                    ))}
-                </CardHeader>
-                <Prompt text={entry.template} />
-            </Card>
+            <div className="entry-list">
+                {this.state.entries.map(renderEntry)}
+            </div>
         )
     }
-    return <div className="entry-list">{data.map(renderEntry)}</div>
 }
 
 export default EntryList
